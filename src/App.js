3238e9248aba82543d0ddcf25e0d43c27123e7f7
Kimleng <141606539+kimlengthai@users.noreply.github.com>
2024-06-19 15:12:48 +1000

    adjust error messages position

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const initialValues = {username:"", email:"", password:""};
+  const initialValues = { username: "", email: "", password: "" };
   const [formValues, setFormValues] = useState(initialValues);
   const [formErrors, setFormErrors] = useState({});
   const [isSubmit, setIsSubmit] = useState(false);
-  
+
   const handleChange = (e) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     /*add a square braces that take username as a key 
       and assign a value to the key */
-    setFormValues({...formValues, [name]: value});
+    setFormValues({ ...formValues, [name]: value });
   };
 
   const handleSubmit = (e) => {
@@ -21,9 +21,9 @@ function App() {
     setIsSubmit(true);
   };
 
-  useEffect (() => {
+  useEffect(() => {
     console.log(formErrors);
-    if(Object.keys(formErrors).length === 0 && isSubmit) {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
       console.log(formValues);
     }
   }, [formErrors, formValues, isSubmit]);
@@ -31,63 +31,63 @@ function App() {
   const validate = (values) => {
     const errors = {};
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-    if(!values.username){
+    if (!values.username) {
       errors.username = "Username is required!";
     }
-    if(!values.email){
+    if (!values.email) {
       errors.email = "Email is required!";
-    }
-    else if(!regex.test(values.email)){
+    } else if (!regex.test(values.email)) {
       errors.email = "This is not a valid email format!";
     }
-    if(!values.password){
+    if (!values.password) {
       errors.password = "Password is required!";
-    }
-    else if(values.password.length < 4){
+    } else if (values.password.length < 4) {
       errors.password = "Password must be more than 4 characters!";
-    }
-    else if(values.password.length > 10){
+    } else if (values.password.length > 10) {
       errors.password = "Password cannot exceed more than 10 characters!";
     }
     return errors;
-  }
+  };
 
   return (
     <div className="container">
-    <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+      <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
       <form onSubmit={handleSubmit}>
         <h1>Login Form</h1>
         <div className='ui divider'></div>
         <div className='ui form'>
           <div className='field'>
-          <p className="error">{formErrors.username}</p>
             <label>Username</label>
-            <input type='text' 
-            name="username" 
-            placeholder='Username' 
-            value={formValues.username}
+            <input
+              type='text'
+              name="username"
+              placeholder='Username'
+              value={formValues.username}
               onChange={handleChange}
             />
+            <p className="error">{formErrors.username}</p>
           </div>
-          <p className="error">{formErrors.email}</p>
           <div className='field'>
             <label>Email</label>
-            <input type='email' 
-            name="email" 
-            placeholder='Email' 
-            value={formValues.email}
-            onChange={handleChange}
+            <input
+              type='email'
+              name="email"
+              placeholder='Email'
+              value={formValues.email}
+              onChange={handleChange}
             />
+            <p className="error">{formErrors.email}</p>
           </div>
-          <p className="error">{formErrors.password}</p>
           <div className='field'>
-          <label>Password</label>
-          <input type='password' 
-          name="password" 
-          placeholder='Password' 
-          value={formValues.password}
-          onChange={handleChange}
-          />
+            <label>Password</label>
+            <input
+              type='password'
+              name="password"
+              placeholder='Password'
+              value={formValues.password}
+              onChange={handleChange}
+            />
+            <p className="error">{formErrors.password}</p>
           </div>
           <button className='fluid ui button blue'>Submit</button>
         </div>
